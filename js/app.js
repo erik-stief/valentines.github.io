1e4a8a2d09869985b27343b81c65bdf9892be74f
erik-stief <estiefeling@outlook.com>
2025-02-13 18:02:12 -0500

    works on pages

--- js/app.js
+++ js/app.js
@@ -58,7 +58,7 @@ yes.addEventListener("click", () => {
   vid.style.display = "block";
   gif.src = "resources/idc.gif";
   text.innerHTML = "Knew it babe 😘";
-  yes.innerHTML = '<a href="DashBoard/dashboard.html">Open Your Gift</a>';
+  yes.innerHTML = '<a href="openGift()">Open Your Gift</a>';
   yes.style.height = "90%";
   yes.style.width = "96%";
   no.style.display = "none";
@@ -66,3 +66,7 @@ yes.addEventListener("click", () => {
     vid.style.display = "none";
   }, 9000);
 });
+
+function openGift() {
+  window.location.href = "DashBoard/dashboard.html";
+}
